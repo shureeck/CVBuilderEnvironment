35fbf25b8bf45ea53197ea3b67368251758854f8
Oleksandr Poliakov <poliakovaleek@gmail.com>
2023-07-26 00:04:13 +0200

    Initial commit

---
+++ test/cv_generator_env.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CvGeneratorEnv from '../lib/cv_generator_env-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cv_generator_env-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CvGeneratorEnv.CvGeneratorEnvStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
